0a96b377c9a84f9e344d9c7912edfe1e3d86d8cc
mdjonayed6 <jonayed2640@gmail.com>
2024-08-19 20:57:35 +0600

    total cost and grand total functionality completed

--- script/app.js
+++ script/app.js
@@ -2,8 +2,41 @@ const allBtn = document.getElementsByClassName("add-btn");
 
 let count = 0;
 for (const btn of allBtn) {
-  btn.addEventListener("click", function () {
+  btn.addEventListener("click", function (e) {
     count = count + 1; //count++
-    document.getElementById("cart-count").innerText = count;
+    const placeName = e.target.parentNode.parentNode.childNodes[1].innerText;
+    const price =
+      e.target.parentNode.parentNode.childNodes[3].childNodes[0].innerText;
+    const selectedContainer = document.getElementById(
+      "selected-place-container"
+    );
+    const li = document.createElement("li");
+    const place = document.createElement("p");
+    place.innerText = placeName;
+    const amount = document.createElement("p");
+    amount.innerText = price;
+
+    li.appendChild(place);
+    li.appendChild(amount);
+    selectedContainer.appendChild(li);
+
+    // Total cost calculation
+    const totalCost = document.getElementById("totalCost").innerText;
+    const convertedTotalCost = parseInt(totalCost);
+    const tourCost = convertedTotalCost + parseInt(price);
+
+    // grand total calculation
+    const grandTotalCost = document.getElementById("grand-total").innerText;
+    const convertedGrandTotalCost = parseInt(grandTotalCost);
+    const grandTourCost = convertedGrandTotalCost + parseInt(price);
+
+    // Display Output
+    setInnerText("cart-count", count);
+    setInnerText("totalCost", tourCost);
+    setInnerText("grand-total", grandTourCost);
   });
+}
+
+function setInnerText(elementId, value) {
+  document.getElementById(elementId).innerText = value;
 }
